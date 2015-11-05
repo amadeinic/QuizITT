78552e021acbb4b719d1213f2a4ac4de0024abcc
Nicola Amadei <nicola.amadei@gmail.com>
2015-11-05 12:51:13 +0100

    Versione consegnata
    Tutto ok, no time

--- QuizITT_WP/QuizITT_WP/MainPage.xaml.cs
+++ QuizITT_WP/QuizITT_WP/MainPage.xaml.cs
@@ -2,6 +2,10 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using System;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -12,39 +16,62 @@ namespace QuizITT_WP
     /// </summary>
 
     public sealed partial class MainPage : Page
-    {
-        public List<Question> Domande { get; set; }
-        public Dictionary<int,string> Categorie { get; set; }
+    {        
         public MainPage()
         {
             this.InitializeComponent();
-
+            
             this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            
-        }
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {            
-
+            //Questo metodo viene chiamato quando la pagina viene visualizzata
+            //Proviamo a recuperare i setting dell'utente, se non li troviamo facciamo senza
+            try
+            {
+                txtImpNome.Text = (string)ApplicationData.Current.LocalSettings.Values["NomeUtente"];
+                txtImpCognome.Text = (string) ApplicationData.Current.LocalSettings.Values["CognomeUtente"];
+                string dataPrelevata = (string) ApplicationData.Current.LocalSettings.Values["DataNascita"];
+                string[] container = dataPrelevata.Split(';');
+                dpkImpData.Date = new DateTime(Convert.ToInt32(container[0]), Convert.ToInt32(container[1]), Convert.ToInt32(container[2]));
+                cmbImpSpec.SelectedIndex = (int)ApplicationData.Current.LocalSettings.Values["Special"];
+                lblCiao.Text= "Ciao "+txtImpNome.Text+"! Bentornato su QuizITT";
+                
+            }
+            catch(Exception)
+            {                
+                //Ci basta gestire l'eccezione, non dobbiamo fare nulla in caso di non recupero, vuol dire solo che è la prima volta che si gioca
+            }
         }
 
         private void btnPlayClassic_Click(object sender, RoutedEventArgs e)
         {
+            //Avviamo la navigazione verso la pagina quiz, passando come parametro la mdoalità scelta
             this.Frame.Navigate(typeof(QuizPage),btnPlayClassic.Content);
         }
 
         private void btnPlayTime_Click(object sender, RoutedEventArgs e)
         {
+            /* Purtroppo al momento non si può giocare in modlaità time attack
+            serve un timer che su windows phone ha una gestione diversa da quella di windows classico e non funziona
+            non appena trovata una soluzione il progetto verrà aggiornato e sarà riscaricabile da github.com/amadeinic nel repository apposito.
+            */
             this.Frame.Navigate(typeof(QuizPage),btnPlayTime.Content);
         }
 
         private void btnPlayInsane_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(QuizPage), btnPlayInsane.Content);
+        }
+
+        private void btnImpostaUtente_Click(object sender, RoutedEventArgs e)
+        {
+            //Alla pressione del bottone salva utente, salviamo i parametri nel dictionary dei settings, messo a disposizione dal sistema operativo
+            ApplicationData.Current.LocalSettings.Values["NomeUtente"] = txtImpNome.Text;
+            ApplicationData.Current.LocalSettings.Values["CognomeUtente"] = txtImpCognome.Text;
+            ApplicationData.Current.LocalSettings.Values["DataNascita"] = dpkImpData.Date.Year + ";" + dpkImpData.Date.Month + ";" + dpkImpData.Date.Day;
+            ApplicationData.Current.LocalSettings.Values["Special"] = cmbImpSpec.SelectedIndex;
         }
     }
 }
--- QuizITT_WP/QuizITT_WP/QuizPage.xaml.cs
+++ QuizITT_WP/QuizITT_WP/QuizPage.xaml.cs
@@ -14,6 +14,7 @@ namespace QuizITT_WP
 {
     public sealed partial class QuizPage : Page
     {
+        //Dentro questa region ci sono le dichiarazioni che servono universalmente
         #region Dichiarazioni
         int domandaCorrente;
         int risposteCorrette = 0;
@@ -35,6 +36,7 @@ namespace QuizITT_WP
             this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
             this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
         }       
+        //Navigation helper è un oggetto che gestisce la navigazione tra le varie pagine (form).
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
@@ -43,15 +45,21 @@ namespace QuizITT_WP
         {
             get { return this.defaultViewModel; }
         }        
+        
         private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
+            //Questo metodo loadstate viene chiamato quando la pagina viene caricata ed è asincrono per permettere gli await
+            lblIndicazione.Visibility = Visibility.Collapsed;
+            //Leggo tutte le domande e le categorie dai file
             string modoQuiz = e.NavigationParameter as string;
             lblIntestazione.Text = modoQuiz;            
             Domande = new List<Question>();
             Categorie = new Dictionary<int, string>();
+            //Espandi questa region per vedere 
             #region Carica domande
             try
             {
+                //await è un metodo asincrono, l'app rimane sospesa finchè la lettura non è completata, per questo è necessario.
                 var Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
                 Folder = await Folder.GetFolderAsync("QuizData");
                 var File = await Folder.GetFileAsync("questions.csv");
@@ -68,14 +76,7 @@ namespace QuizITT_WP
                 foreach (string st in lines)
                 {
                     Categorie.Add(Convert.ToInt32(st.Split(';')[0]), st.Split(';')[1]);
-                }
-                //foreach (Question q in Domande)
-                //{
-                //    if (Categorie.ContainsKey(q.Category))
-                //    {
-
-                //    }
-                //}
+                }               
 
             }
             catch (Exception exc)
@@ -84,6 +85,9 @@ namespace QuizITT_WP
                 await msg.ShowAsync();
             }
             #endregion
+
+            //Se scelgo un time attack allora preparo tutte le domande per la visualizzazione (al momento non si può usare perchè manca il timer)
+            //Altrimenti nelle altre due modalità preparo le 10 domande che saranno proposte.
             if(modoQuiz!="Time Attack")
             {
                 int quanteDomande = 10;
@@ -96,51 +100,57 @@ namespace QuizITT_WP
                     {
                         i--;
                     }
-                    else                    
-                        for (int a = 0; a < i; a++)                        
+                    else
+                        for (int a = 0; a < i; a++)
                             if (domandeScelte[a] == domandeScelte[i])
                             {
                                 //controllo se la domanda è già presente
                                 i--;
                                 break;
                             }
-                }               
+                }
             }
             else
             {
-                int quanteDomande = Domande.Count;
-                domandeScelte = new Question[quanteDomande];
-                for (int i = 0; i < quanteDomande; i++)
-                {
-                    domandeScelte[i] = Domande[r.Next(0, Domande.Count)];
+                //NON ANCORA PRONTO perchè manca il timer, il resto va.
+                throw new NotImplementedException();
 
-                    for (int a = 0; a < i; a++)
-                        if (domandeScelte[a] == domandeScelte[i])
-                        {
-                            //controllo se la domanda è già presente
-                            i--;
-                            break;
-                        }
-                }
+
+                //int quanteDomande = Domande.Count;
+                //domandeScelte = new Question[quanteDomande];
+                //for (int i = 0; i < quanteDomande; i++)
+                //{
+                //    domandeScelte[i] = Domande[r.Next(0, Domande.Count)];
+
+                //    for (int a = 0; a < i; a++)
+                //        if (domandeScelte[a] == domandeScelte[i])
+                //        {
+                //            //controllo se la domanda è già presente
+                //            i--;
+                //            break;
+                //        }
+                //}
                 //preparo attack con timer
-                //dt.Tick += new EventHandler(dt_Tick);
+                //dt.Tick += new EventHandler(dt_Tick);  <------ questa è la parte da modificare
                 //dt.Interval = new TimeSpan(0, 0, 1);
                 //dt.Start();
 
             }
-            //In ogni caso
+            //In ogni caso visualizzo la prima domanda
             VisualizzaDomanda();
         }
 
-        private void dt_Tick(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
+        //private void dt_Tick(object sender, EventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        
         
         private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
+            //Questo metodo verrebbe chiamato all'uscita dalla page, non ci mettiamo niente perchè non ci serve.
         }
-        
+
         private void VisualizzaDomanda()
         {
             Question miaDomanda = domandeScelte[domandaCorrente];
@@ -150,6 +160,7 @@ namespace QuizITT_WP
             btnR1.Content = miaDomanda.Answers[1].Text;
             btnR2.Content = miaDomanda.Answers[2].Text;
             lblTitolo.Text = Categorie[miaDomanda.Category];
+            //Poichè ho visualizzato una nuova domanda l'utente deve nuovamente rispondere, resetto quindi il bottone.
             bottoneCliccato = false;
         }
 
@@ -170,9 +181,11 @@ namespace QuizITT_WP
         #endregion
         private async void btnR_Click(object sender, RoutedEventArgs e)
         {
+            //Metodo chiamato da tutti e 3 i bottoni
             Button btn = sender as Button;
             if (!bottoneCliccato)
             {
+                lblIndicazione.Visibility = Visibility.Visible;
                 bottoneCliccato = true;
                 foreach (Answer risposta in domandeScelte[domandaCorrente].Answers)
                 {
@@ -186,6 +199,7 @@ namespace QuizITT_WP
                         }
                         else
                         {
+                            //Visualizzo la risposta che era giusta e coloro quella scelta di rosso
                             btn.Background = new SolidColorBrush(Windows.UI.Colors.Red);
                             if ((string)btnR0.Content == risposta.Text)
                                 btnR0.Background = new SolidColorBrush(Windows.UI.Colors.Green);
@@ -199,6 +213,7 @@ namespace QuizITT_WP
             }
             else
             {
+                lblIndicazione.Visibility = Visibility.Collapsed;
                 btnR0.Background = null;
                 btnR1.Background = null;
                 btnR2.Background = null; 
